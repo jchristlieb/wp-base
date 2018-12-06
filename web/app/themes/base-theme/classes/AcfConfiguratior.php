a2e2ee75f29b00e9eb1243ef93306057954a331d
Jan Christlieb <mail@janchristlieb.de>
2018-12-06 09:40:05 +0100

    add custom theme classes

---
+++ web/app/themes/base-theme/classes/AcfConfiguratior.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BaseTheme;
+
+class AcfConfiguratior
+{
+    public function __construct()
+    {
+        add_action('acf/init', [$this, 'add_google_api_key']);
+    }
+
+    public function add_google_api_key()
+    {
+        acf_update_setting('google_api_key', getenv('GOOGLE_MAPS_API_KEY'));
+    }
+}
--- web/app/themes/base-theme/classes/AssetHandler.php
+++ web/app/themes/base-theme/classes/AssetHandler.php
@@ -6,5 +6,18 @@ class AssetHandler
 {
     public function __construct()
     {
+        add_action('wp_enque_scripts', [$this, 'add_css']);
+        add_action('wp_enque_scripts', [$this, 'add_js']);
+    }
+
+    public function add_js()
+    {
+        wp_enqueue_script('bootstrap_js', get_template_directory_uri() . '/assets/lib/bootstrap.min.js', ['jquery'], '1.0.0', true);
+        wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/main.js', ['jquery'], false, true);
+    }
+
+    public function add_css()
+    {
+        wp_enqueue_style('app', get_template_directory_uri() . '/assets/css/main.css');
     }
 }
